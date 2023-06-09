a8eaa8cc34c8de78a864b103d9df30624e6c29e8
EMMANUELLA10 <84028425+EMMANUELLA10@users.noreply.github.com>
2023-06-09 18:40:22 +0000

    find_the_difference.py

--- find_the_difference.py
+++ find_the_difference.py
@@ -1,10 +1,5 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        sl= list(s)
-        tl = list(t)
-        sl.sort()
-        tl.sort()
-        n = len(sl)
-        for i in range(n):
-            tl.remove(sl[i])
-        return tl[0]
+        for i in t:
+            if t.count(i) != s.count(i):
+                return i
