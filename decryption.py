81d315936520e62259b9a4b62889bf33e2aa1490
EMMANUELLA10 <84028425+EMMANUELLA10@users.noreply.github.com>
2023-06-13 09:44:30 +0000

    Add files via upload

---
+++ decryption.py
@@ -0,0 +1,12 @@
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        res = deque()
+        r = len(s) - 1
+        while r >= 0:
+            if s[r] == "#":
+                res.appendleft(chr(int(s[r-2:r])+ord("a")-1))
+                r-=3
+            else:
+                res.appendleft(chr(int(s[r])+ord("a")-1))
+                r-=1
+        return "".join(res)
